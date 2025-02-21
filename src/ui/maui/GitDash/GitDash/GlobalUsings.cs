55f2224c84ac5be71d70d7856e28195e85a7924a
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2025-02-21 17:32:28 -0500

    remove maui

--- src/ui/maui/GitDash/GitDash/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Fonts;
-global using GitDash.Data;
-global using GitDash.PageModels;
-global using GitDash.Pages;
-global using GitDash.Services;
-global using GitDash.Utilities;
--- src/ui/maui/GitDash/GitDash/Models/ProjectsTags.cs
+++
@@ -1,9 +0,0 @@
-namespace GitDash.Models
-{
-    public class ProjectsTags
-    {
-        public int ID { get; set; }
-        public int ProjectID { get; set; }
-        public int TagID { get; set; }
-    }
-}
